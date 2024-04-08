3a358ea20a7b5777b3487b8939549237691a2dbd
Igor Terriaga <igsantos1996@gmail.com>
2024-04-08 16:42:59 -0300

    add pagination

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -6,6 +6,10 @@ import com.example.springboot.repositories.IProductRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,24 +35,24 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
 
+
     @GetMapping("/products")
-    public ResponseEntity<List<ProductModel>> getAllProducts() {
-        List<ProductModel> productList = productRepository.findAll();
-        if (!productList.isEmpty()) {
-            for (ProductModel product : productList) {
-                UUID id = product.getIdProduct();
-                product.add(linkTo(methodOn(ProductController.class).getOneProducts(id)).withSelfRel());
-            }
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+    public ResponseEntity<Page<ProductModel>> getAllProducts(@PageableDefault(page = 0, size = 10, sort = "idProduct", direction = Sort.Direction.ASC) Pageable pageable) {
+        Page<ProductModel> productsPage = productRepository.findAll(pageable);
+        productsPage.forEach(product -> product.add(linkTo(methodOn(ProductController.class).getOneProducts(product.getIdProduct())).withSelfRel()));
+        return ResponseEntity.status(HttpStatus.OK).body(productsPage);
     }
 
     @GetMapping("/products/{id}")
     public ResponseEntity<Object> getOneProducts(@PathVariable(value = "id") UUID id) {
-        Optional<ProductModel> productO = productRepository.findById(id);
-        productO.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withSelfRel());
 
-        return productO.<ResponseEntity<Object>>map(productModel -> ResponseEntity.status(HttpStatus.OK).body(productModel)).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found"));
+        Optional<ProductModel> productO = productRepository.findById(id);
+        if (productO.isPresent()) {
+            productO.get().add(linkTo(methodOn(ProductController.class).getOneProducts(id)).withSelfRel());
+            return ResponseEntity.status(HttpStatus.OK).body(productO.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
     }
 
     @DeleteMapping("/products/{id}")
--- src/main/java/com/example/springboot/repositories/IProductRepository.java
+++ src/main/java/com/example/springboot/repositories/IProductRepository.java
@@ -1,6 +1,8 @@
 package com.example.springboot.repositories;
 
+import org.springframework.data.domain.Pageable;
 import com.example.springboot.models.ProductModel;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface IProductRepository extends JpaRepository<ProductModel, UUID> {
+//    Page<ProductModel> findAll(Pageable pageable);
 }
